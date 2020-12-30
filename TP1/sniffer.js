c6721748f043af5c1f0a44c2b340bc3e604bede9
GuiguiSo <guillaume.sonnery@ecole-isitech.fr>
2020-12-30 11:34:19 +0100

    TP1 en cours

--- TP1/sniffer.js
+++ TP1/sniffer.js
@@ -1,1 +1,32 @@
-console.log(req.connection.remotePort);
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+  
+const http = require('http'); 
+
+for (i = 3000; i <= 4000; i++) {
+    const optionRequestPort = {host:'127.0.0.1', port: i, path: '/ping'};
+
+    http.get(optionRequestPort, (res) => {
+
+        const { statusCode } = res;
+        const contentType = res.headers['content-type'];
+      
+        let error;
+        if (statusCode !== 200) {
+          error = new Error('Request Failed.\n' +
+                            `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+          error = new Error('Invalid content-type.\n' +
+                            `Expected application/json but received ${contentType}`);
+        }
+        if (error) {
+          console.error(error.message);
+          res.resume();
+          return;
+          } else {
+              console.log(`le port trouver est ${optionRequestPort.port}`);
+          }
+      }).on('error', (e) => {});
+}
